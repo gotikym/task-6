ada6e57d4b22188ce5c0e42df748c5bf39f145ff
One <One@DESKTOP-MIB22JC>
2022-04-17 21:59:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        var pictures = 52;
+        var series = 3;
+        var numberSeries = pictures / series;
+        var overflow = pictures % series;
+        Console.WriteLine($"Полностью заполненных рядов: {numberSeries}, картинок сверх меры: {overflow}");
+        }
+    }
